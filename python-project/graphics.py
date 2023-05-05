62fa46027fcb4a1ee3014f5701dfc552aefd7c1b
Simone Masullo <masullo.simone00@gmail.com>
2023-05-05 18:28:25 +0200

    Added feature extraction tecniques

--- python-project/graphics.py
+++ python-project/graphics.py
@@ -39,30 +39,14 @@ def show_graphic(raw, column_list = None, min_age=0, max_age=-1):
 if __name__ == '__main__':
     raw = pr.load(pr.PATH_TO_DS_1)
     #matplotlib.use('TkAgg')    # Uncomment if you want to set the display controller for matplotlib
-    show_graphic(raw, column_list=["F3", "FC6"])
-    raw = pr.bandpass_filter(raw, l_freq=0.5)
-    show_graphic(raw, column_list=["F3", "FC6"])
 
-"""
-ar = AutoReject()
-segment_len = 3.0
-n_segments = int(np.floor(raw.times[-1] / segment_len))     #calcola il numero di segmenti
-segments = np.array_split(raw.get_data(), n_segments, axis=1)   #suddivisione del segnale
-# Crea una lista di oggetti RawArray
-raw_arrays = []
-for i, segment_data in enumerate(segments):
-    info = mne.create_info(ch_names=raw.ch_names, sfreq=raw.info['sfreq'], ch_types='eeg')
-    raw_arrays.append(mne.io.RawArray(segment_data, info))
+    # Remove 50Hz frequency (electrical noise)
+    raw = raw.notch_filter(50, picks='eeg')
 
-raw_arrays = raw_arrays.real()
-raw = mne.io.RawArray(raw_arrays, info)
-# Crea gli oggetti Epochs
-epochs = mne.Epochs(raw_arrays, events=None, tmin=0, tmax=segment_len, baseline=None)
-#epochs = mne.make_fixed_length_epochs(raw, duration=3.0)    #conversione da raw a epoch, per dare in input a trasform
-ar.fit(raw)
-clean_raw, _ = ar.transform(raw, return_log=True)
-raw.plot()
-clean_raw.plot()
-print(_)
-"""
+    raw = raw.filter(l_freq= 0.5, h_freq= 120)
 
+    # Remove eye blinking and muscle movement
+    raw = pr.ica_processing(raw, excluded_list=[0])
+
+    # Compute spectral features
+    spectral_features, spectral_features_names = pr.compute_spectral_features(raw)
--- python-project/preprocessing.py
+++ python-project/preprocessing.py
@@ -1,7 +1,9 @@
 import scipy
 import mne
-from mne.preprocessing import ICA
 import pandas as pd
+import numpy as np
+from mne.preprocessing import ICA
+from mne.time_frequency import psd_array_multitaper, morlet
 
 # Setting up
 
@@ -27,16 +29,6 @@ def load(path):
     raw = mne.io.RawArray(df.values.T, mne_info)
     return raw
 
-def bandpass_filter(raw, l_freq=0.5, h_freq=50):
-    """
-    Applies the band pass filter to the raw dataset given
-    :param raw: the mne RAW data structure to filter
-    :param l_freq: the low limit for the filter
-    :param h_freq: the high limit for the filter
-    :return: returns the filtered data structure
-    """
-    return raw.filter(l_freq=4, h_freq=50)
-
 def ica_processing(raw, excluded_list=None):
     """
     Applies the ICA processing method to the raw data structure ignoring the component given as excluded_list
@@ -44,8 +36,67 @@ def ica_processing(raw, excluded_list=None):
     :param excluded_list: the list of sensors to exclude
     :return: the raw data structure after applying the filter
     """
-    ica = ICA(method='infomax', n_components=16, random_state=0)
+    ica = ICA(n_components=14, random_state=0)
     ica.fit(raw)
     ica.exclude = excluded_list
     ica.apply(raw)
     return raw
+
+# W.I.P.
+def compute_arcc(raw, segment_len=3.0):
+    """
+    Compute Auto Regressive C Coefficients
+    :param raw:
+    :return: the calculated coefficients
+    """
+    # ar = AutoReject()
+    # segment_len = 3.0
+    # n_segments = int(np.floor(raw.times[-1] / segment_len))     #calcola il numero di segmenti
+    # segments = np.array_split(raw.get_data(), n_segments, axis=1)   #suddivisione del segnale
+    #
+    # # Crea una lista di oggetti RawArray
+    # raw_arrays = []
+    # for i, segment_data in enumerate(segments):
+    #     info = mne.create_info(ch_names=raw.ch_names, sfreq=raw.info['sfreq'], ch_types='eeg')
+    #     raw_arrays.append(mne.io.RawArray(segment_data, info))
+    #
+    # raw_arrays = raw_arrays.real()
+    # raw = mne.io.RawArray(raw_arrays, info)
+    # # Crea gli oggetti Epochs
+    # epochs = mne.Epochs(raw_arrays, events=None, tmin=0, tmax=segment_len, baseline=None)
+    # #epochs = mne.make_fixed_length_epochs(raw, duration=3.0)    #conversione da raw a epoch, per dare in input a trasform
+    # ar.fit(raw)
+    # clean_raw, _ = ar.transform(raw, return_log=True)
+    # raw.plot()
+    # clean_raw.plot()
+    # print(_)
+    # return clean_raw
+
+def compute_spectral_features(raw):
+    """
+    Computes the spectral features for the raw object provided
+    :param raw: the raw data structure
+    :return: the spectral features and the names of the extracted features
+    """
+
+    # Compute PSD using multitaper method from mne
+    psd, freqs = psd_array_multitaper(raw.get_data(), sfreq=raw.info['sfreq'])
+
+    # Compute spectral features
+    spectral_centroid = np.sum(psd * freqs[:, np.newaxis].T, axis=0) / np.sum(psd, axis=0)
+    spectral_entropy = -np.sum(psd * np.log2(psd), axis=0)
+    spectral_edge_frequency = np.sum(np.cumsum(psd, axis=0) <= 0.5, axis=0) / psd.shape[0] * raw.info['sfreq'] / 2.
+    spectral_flatness = np.exp(np.mean(np.log(psd), axis=0)) / np.mean(psd, axis=0)
+
+    # Concatenate results
+    spectral_features = np.concatenate((spectral_centroid, spectral_entropy, spectral_edge_frequency, spectral_flatness))
+    spectral_features_names = np.array(("Spectral centroid", "Spectral entropy", "Spectral edge frequency", "Spectral flatness"))
+
+    return spectral_features, spectral_features_names
+
+def compute_wavelet_transform(raw):
+    """
+    Computes the wavelet transform for the raw structure provided
+    :param raw: the mne data structure providing the measurements to compute the wavelet transform
+    :return: the wavelet features extracted and their names
+    """
