4b3cce24ea66634216bcfc00fcdd31de6b8a8f3f
Simone Masullo <masullo.simone00@gmail.com>
2023-05-11 10:34:05 +0200

    Adding ICA\nLoading all measurements

--- python-project/graphics.py
+++ python-project/graphics.py
@@ -1,59 +1,29 @@
-import mne
 import preprocessing as pr
 import matplotlib.pyplot as plt
 
-def show_graphic(raw, column_list = None, min_age=0, max_age=-1):
+def show_graphic(df, column_list = None, min_age=0, max_age=-1):
     """
-    Shows the graphic of the input mne RAW DataStructure
-    :param raw: the mne raw data structure containing the measurements
+    Shows the graphic of the input raw data structure
+    :param raw: the raw data structure containing the measurements
     :param column_list: the list of columns to be put in the graphics
     :param min_age: the starting time to show the measurements
     :param max_age: the last time to show the measurements
     :return: None
     """
-    # If no column list is provided show the first 7 columns (in order to not create a messy graphic)
-
-    if column_list is None:
-        column_list = raw.ch_names[2:10]
-
-    to_show = mne.pick_channels(raw.ch_names, column_list)
-
-    # Select the sub-interval of measurements
-    start, stop = raw.time_as_index([raw.times[min_age], raw.times[max_age]])
-    data, times = raw[to_show, start:stop]
-
-    # Add legend to the graphic
-
-    fig, ax = plt.subplots()
-    ax.plot(times, data.T)
-
-    for idx, pick in enumerate(to_show):
-        ax.plot(raw.times, raw._data[pick], label=column_list[idx])
+    # Close all plots if there are any
+    plt.close("all")
 
     # Show the graphic
-
-    ax.set(xlabel="Time (s)", ylabel = "Frequency (mV)", title="EEG Data")
-    ax.legend()
+    df.plot(x="UNIX_TIMESTAMP")
     plt.show()
 
 if __name__ == '__main__':
-    raw = pr.load(pr.PATH_TO_DS_1)
+    df = pr.load(pr.PATH_TO_DS_1)
     #matplotlib.use('TkAgg')    # Uncomment if you want to set the display controller for matplotlib
 
-    # Remove 50Hz frequency (electrical noise)
-    raw = raw.notch_filter(50, picks='eeg')
+    # # Apply a butterworth bandpass filter
+    # df = pr.bandpass_filter(df)
+    #
+    # # Apply indipendent component analysis to reduce the complexity of the signal
+    # ica_values = pr.ica_processing(df, 16)
 
-    raw = raw.filter(l_freq= 0.5, h_freq= 120)
-
-    # Remove eye blinking and muscle movement
-    raw = pr.ica_processing(raw, excluded_list=[0])
-
-    # Compute spectral features
-    spectral_features, spectral_features_names = pr.compute_spectral_features(raw)
-
-    # Compute wavelet features
-    # wavelet_features, wavelet_features_names = pr.compute_wavelet_transform(raw)
-
-    show_graphic(raw)
-    plt.plot(spectral_features)
-    plt.show()
--- python-project/preprocessing.py
+++ python-project/preprocessing.py
@@ -1,120 +1,120 @@
 import scipy
-import mne
 import pandas as pd
-import numpy as np
-from mne.preprocessing import ICA
-from mne.time_frequency import psd_array_multitaper
-from mne.time_frequency import tfr_array_morlet
+from scipy import signal
+from sklearn.decomposition import FastICA
+
+
 
 # Setting up
 
 PATH_TO_DS_1 = "/home/smasu/Documents/FVAB/BED_Biometric_EEG_dataset/BED_Biometric_EEG_dataset/BED/RAW_PARSED/"
 PATH_TO_DS_2 = "/Users/mattiadargenio/Desktop/Unisa/Corsi/1:2/biometria/ProgettoEEG/BED_Biometric_EEG_dataset/BED/RAW_PARSED/"
 
-def load(path):
+def load(path, samp_freq=256):
     """
-    Loads the matlib dataset into mne RAW data structures
+    Loads the matlib dataset into a pandas data stracture
     :param path: the path to the dataset
-    :return: the mne RAW data structure
+    :return: the raw data structure
     """
-    dataset = scipy.io.loadmat(path+"s1_s1.mat")
-
     # Loading 18 labels as given in the instructions
 
-    labels = ['COUNTER', 'INTERPOLATED', 'F3', 'FC5', 'AF3', 'F7', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'F8', 'AF4', 'FC6', 'F4', 'UNIX_TIMESTAMP']
-    recordings = dataset['recording']
-    df = pd.DataFrame(recordings, columns=labels)
+    labels = ['COUNTER',
+              'INTERPOLATED',
+              'F3',
+              'FC5',
+              'AF3',
+              'F7',
+              'T7',
+              'P7',
+              'O1',
+              'O2',
+              'P8',
+              'T8',
+              'F8',
+              'AF4',
+              'FC6',
+              'F4',
+              'UNIX_TIMESTAMP']
 
-    # Creating mne RAW data structure
-    mne_info = mne.create_info(ch_names=labels, sfreq=1000, ch_types="eeg")
-    raw = mne.io.RawArray(df.values.T, mne_info)
+    recordings = []
 
-    return raw
+    for i in range(1, 22):
+        for j in range(1, 4):
+            dataset_path = "{0}s{1}_s{2}.mat".format(path, i, j)
+            dataset = scipy.io.loadmat(dataset_path)
+            recordings.extend(dataset['recording'])
 
-def ica_processing(raw, excluded_list=None):
+    print("Final length: {}".format(len(recordings)))
+
+    # df = pd.DataFrame(recordings, columns=labels)
+    # df = df.drop(["INTERPOLATED"], axis=1)
+
+    return df
+
+def bandpass_filter(df, low_value=0.5, high_value=50, sampling_frequency=256, order=5):
     """
-    Applies the ICA processing method to the raw data structure ignoring the component given as excluded_list
-    :param raw: the mne RAW data structure
+    Filters the values of a dataframe given a low_value and a high_value
+    :param df: the raw datastructure
+    :param low_value: the lowest bound
+    :param high_value: the highest bound
+    :return: the filtered dataframe
+    """
+    # Create a new DataFrame for containing the filtered values
+    filtered = pd.DataFrame()
+
+    # Do not apply the filter on the following columns
+    to_skip = ["COUNTER", "UNIX_TIMESTAMP"]
+
+    for column in to_skip:
+        filtered[column] = df[column]
+
+    for column in df.columns:
+        # Skip these columns
+        if column in to_skip:
+            continue
+
+        # Create the butterworth filter
+        num, den = signal.butter(N=order, Wn=(low_value, high_value), btype='bandpass', analog=False, fs=sampling_frequency)
+
+        # Apply the filter
+        filtered[column] = signal.filtfilt(num, den, df[column])
+
+    return filtered
+
+
+def ica_processing(df, n_components=None):
+    """
+    Applies the ICA processing method to the raw data structure in order to reduce the number of components
+    :param df: the raw data structure
     :param excluded_list: the list of sensors to exclude
     :return: the raw data structure after applying the filter
     """
-    ica = ICA(n_components=14, random_state=0)
-    ica.fit(raw)
-    ica.exclude = excluded_list
-    ica.apply(raw)
-    return raw
+    ICA = FastICA(n_components=n_components, whiten='unit-variance')
+    ica_values = ICA.fit_transform(df.values)
 
-# W.I.P.
-def compute_arcc(raw, segment_len=3.0):
+    return ica_values
+
+
+def compute_arrc(raw, segment_len=3.0):
     """
-    Compute Auto Regressive C Coefficients
-    :param raw:
+    Compute Auto Regressive Coefficients
+    :param raw: the raw data structure
     :return: the calculated coefficients
     """
-    # ar = AutoReject()
-    # segment_len = 3.0
-    # n_segments = int(np.floor(raw.times[-1] / segment_len))     #calcola il numero di segmenti
-    # segments = np.array_split(raw.get_data(), n_segments, axis=1)   #suddivisione del segnale
-    #
-    # # Crea una lista di oggetti RawArray
-    # raw_arrays = []
-    # for i, segment_data in enumerate(segments):
-    #     info = mne.create_info(ch_names=raw.ch_names, sfreq=raw.info['sfreq'], ch_types='eeg')
-    #     raw_arrays.append(mne.io.RawArray(segment_data, info))
-    #
-    # raw_arrays = raw_arrays.real()
-    # raw = mne.io.RawArray(raw_arrays, info)
-    # # Crea gli oggetti Epochs
-    # epochs = mne.Epochs(raw_arrays, events=None, tmin=0, tmax=segment_len, baseline=None)
-    # #epochs = mne.make_fixed_length_epochs(raw, duration=3.0)    #conversione da raw a epoch, per dare in input a trasform
-    # ar.fit(raw)
-    # clean_raw, _ = ar.transform(raw, return_log=True)
-    # raw.plot()
-    # clean_raw.plot()
-    # print(_)
-    # return clean_raw
+
 
 def compute_spectral_features(raw):
     """
-    Computes the spectral features for the raw object provided
+    Computes the spectral features for the raw data structure provided
     :param raw: the raw data structure
     :return: the spectral features and the names of the extracted features
     """
 
-    # Compute PSD using multitaper method from mne
-    psd, freqs = psd_array_multitaper(raw.get_data(), sfreq=raw.info['sfreq'])
-
-    # Compute spectral features
-    spectral_centroid = np.sum(psd * freqs[:, np.newaxis].T, axis=0) / np.sum(psd, axis=0)
-    spectral_entropy = -np.sum(psd * np.log2(psd), axis=0)
-    spectral_edge_frequency = np.sum(np.cumsum(psd, axis=0) <= 0.5, axis=0) / psd.shape[0] * raw.info['sfreq'] / 2.
-    spectral_flatness = np.exp(np.mean(np.log(psd), axis=0)) / np.mean(psd, axis=0)
-
-    # Concatenate results
-    spectral_features = np.concatenate((spectral_centroid, spectral_entropy, spectral_edge_frequency, spectral_flatness))
-    spectral_features_names = np.array(("Spectral centroid", "Spectral entropy", "Spectral edge frequency", "Spectral flatness"))
-
-    return spectral_features, spectral_features_names
 
 def compute_wavelet_transform(raw):
     """
-    Computes the wavelet transform for the raw structure provided
+    Computes the wavelet transform for the raw data structure provided
     :param raw: the mne data structure providing the measurements to compute the wavelet transform
     :return: the wavelet features extracted and their names
     """
-    # Setup the wavelet parameters
-    sfreq = raw.info['sfreq']
-    freqs = [10, 20, 30]
-    n_cycles = 5
 
-    # Compute the wavelet transform
-    tfr = tfr_array_morlet(raw.get_data(), sfreq=sfreq, freqs=freqs, n_cycles=n_cycles)
-
-    # Compute wavelet features
-    mean_energy = np.mean(np.abs(tfr) ** 2, axis=2)
-
-    # Concatenate results
-    wavelet_features = np.concatenate((mean_energy))
-    wavelet_features_names = np.array(['Mean energy'])
-
-    return wavelet_features, wavelet_features_names
