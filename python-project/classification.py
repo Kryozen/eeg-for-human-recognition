29be38e603cdea9ec2e83c4c305946ab8bdf28cf
Mattia d'Argenio <95039085+mattiadarg@users.noreply.github.com>
2023-06-21 15:39:06 +0200

    Update classification.py

--- python-project/classification.py
+++ python-project/classification.py
@@ -1,27 +1,19 @@
-# Import module math for general purpose
 import math
 
-# Import module tqdm for progress bar utility
+from sklearn.ensemble import RandomForestClassifier
 from tqdm import tqdm
 
-# Import module numpy for general purpose
 import numpy as np
-
-# Import module tensorflow for machine learning and deep learning algorithms
 import tensorflow as tf
-
-# Import module tensorflow decision forests for Random Forest algorithm
 import tensorflow_decision_forests as tfdf
-
-# Import module xgboost for better performing algorithms
 import xgboost as xgb
 
-# Import module scikitlearn for scaling data
 from sklearn.preprocessing import MinMaxScaler
-
-# Import module keras for LSTM algorithm
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
+
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+
 
 def train_test_split(users_measurements, perc_train=70):
     """
@@ -33,22 +25,17 @@ def train_test_split(users_measurements, perc_train=70):
     # Create an instance of a scaler
     scaler = MinMaxScaler(feature_range=(0, 1))
 
-    # For each subject, scale data in order to have them between 0 and 1
+    # Scale all the data to be values between 0 and 1
     for user_measurement in users_measurements:
-
-        # Apply scaling
         new_values = scaler.fit_transform(user_measurement.values)
-
-        # Change subject data with the scaled ones
         user_measurement.values = new_values
 
-    # Create lists of tuples (m, i) where m is a numpy array and i is the id of the subject (source of measurements).
+    # Creating lists of tuples (m, i) where m is a numpy array and i is the id of the subject (source of measurements).
     train_data = []
     test_data = []
 
     # For each subject split into train and test
     for user_measurement in users_measurements:
-
         # Calculate the number of measurements to use for training
         training_dataset_length = math.ceil(len(user_measurement) * perc_train / 100)
 
@@ -66,67 +53,57 @@ def train_test_split(users_measurements, perc_train=70):
         test_data_0 = data_set[training_dataset_length:]
         test_data.append((test_data_0, user_measurement.subject_id))
 
-    # Create a list of measurements (x_train) and a list of labels (y_train) for training data
+    # Creating a list of measurements (x_train) and a list of labels (y_train) for training data
     x_train = train_data[0][0]
     y_train = []
 
-    # Append one label for each measurement of the first session
+    # Appending one label for each measurement of the first session
     for _ in range(len(x_train)):
         y_train.append(train_data[0][1])
 
-    # Append the other two sessions
+    # Appending the other two sessions
     for single_user in train_data[1:]:
-        # Reshape the list in order to always have the same number of columns
         train_0 = (np.delete(single_user[0], np.s_[-1:], axis=1), single_user[1])
-
-        # Append the measurements to the list of training data
         x_train = np.concatenate((x_train, train_0[0]))
-
-        # Append the subject label for each measurement
+        # Appending one label for each measurement
         for _ in range(len(train_0[0])):
             y_train.append(single_user[1])
 
-    # Convert to a numpy array
+    # Convert to numpy array
     y_train = np.array(y_train)
 
-    # Create a list of measurements (x_test) and a list of labels (y_test) for test data
+    # Creating a list of measurements (x_test) and a list of labels (y_test) for test data
     x_test = test_data[0][0]
     y_test = []
 
-    # Append one label for each measurement of the first session
+    # Appending one label for each measurement of the first session
     for _ in range(len(x_test)):
         y_test.append(test_data[0][1])
 
-    # Append the other two sessions
     for single_user in test_data[1:]:
-        # Reshape the list in order to always have the same number of columns
         test_0 = (np.delete(single_user[0], np.s_[-1:], axis=1), single_user[1])
-
-        # Append the measurements to the list of testing data
         x_test = np.concatenate((x_test, test_0[0]))
-
-        # Append the subject label for each measurement
         for _ in range(len(test_0[0])):
             y_test.append(single_user[1])
 
-    # Convert to a numpy array
+    # Convert y_test to a numpy array
     y_test = np.array(y_test)
 
-    # Shuffle data so that the algorithm doesn't train on the order of subject
+    # Shuffling data
 
-    ## Zip the arrays together to keep coherence
+    # Zip the arrays together to keep coherence
     train_zip = list(zip(x_train, y_train))
     test_zip = list(zip(x_test, y_test))
 
-    ## Shuffle zipped lists
+    # Shuffle zipped lists
     np.random.shuffle(train_zip)
     np.random.shuffle(test_zip)
 
-    ## Unzip lists
+    # Unzip lists
     x_train, y_train = zip(*train_zip)
     x_test, y_test = zip(*test_zip)
 
-    ## Convert back to numpy arrays
+    # Convert back to numpy arrays
     x_train, y_train, x_test, y_test = np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
 
     return x_train, y_train, x_test, y_test
@@ -155,24 +132,17 @@ def classification_by_random_forest(x_train, y_train, grid_search = False):
     Trains a model using the random forest algorithm.
     :param x_train: the measurements for the training
     :param y_train: the labels for the training
-    :param grid_search: enable the grid search for hyperparameters computation (slower - default False)
     :return: the trained model
     """
-
-    # Define the new model
     model = None
-
-    # Check the grid_search parameter
     if grid_search:
         # Create a tuner
         tuner = tfdf.tuner.RandomSearch(num_trials=10)
 
         # Number of trees in random forest
         n_estimators = [int(x) for x in np.linspace(start=200, stop=500, num=10)]
-
         # Maximum number of levels in tree
         max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-
         # Method of selecting samples for training each tree
         bootstrap = [True, False]
 
@@ -181,36 +151,28 @@ def classification_by_random_forest(x_train, y_train, grid_search = False):
         tuner.choice("max_depth", max_depth)
         tuner.choice("bootstrap_training_dataset", bootstrap)
 
-        # Create a tuned random forest model
-        model = tfdf.keras.RandomForestModel(tuner=tuner)
-
-        # Fit the random search model
+        # Create a tuned model
+        model = tfdf.keras.RandomForestModel(tuner=tuner)  # Fit the random search model
         model.fit(x_train, y_train)
     else:
-        # Create a simple random forest model
         model = tfdf.keras.RandomForestModel()
-
-        # Fit the random forest model
         model.fit(x_train, y_train, verbose=1)
 
     return model
 
 def prediction_by_random_forest(model, x_test):
     """
-    Makes predictions given a model trained with a random forest algorithm
-    :param model: the pre-trained model
-    :param x_test: the measurements of the test portion of the dataset
-    :return: the predictions
-    """
-
+        Makes predictions given a model trained with a random forest algorithm
+        :param model: the pre-trained model
+        :param x_test: the measurements of the test portion of the dataset
+        :return: the predictions
+        """
     # Make predictions
+    print("\n## INFO: starting predictions")
     predictions = model.predict(x_test, verbose=1)
 
-    # Round values to the nearest integer
     predictions = np.round(predictions)
 
-    return predictions
-    # Create the list of outputs
     predictions_0 = []
 
     # Adjust values
@@ -220,15 +182,70 @@ def prediction_by_random_forest(model, x_test):
 
     return predictions_0
 
+
+def classification_by_gridsearch(x_train, y_train, grid_search=False):
+    """
+
+    :param x_train:
+    :param y_train:
+    :return:
+    """
+    if grid_search:
+
+        param_grid = {
+            'n_estimators': [10, 50, 100],
+            'max_depth': [None, 5, 10],
+            'criterion': ['gini', 'entropy']
+        }
+
+        rfc = RandomForestClassifier()
+        grid_search = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+        grid_search.fit(x_train, y_train)
+        print("Migliori parametri:", grid_search.best_params_)
+        print("Miglior punteggio di validazione incrociata:", grid_search.best_score_)
+        print("Miglior modello trovato:", grid_search.best_estimator_)
+
+    else:
+        print("else")
+
+
+def classification_by_randomfgridsearch(x_train, y_train):
+    """
+
+    :param x_train:
+    :param y_train:
+    :return:
+    """
+    model = RandomForestClassifier(n_estimators=100, max_depth=None, criterion='gini')
+    model.fit(x_train, y_train)
+    return model
+
+
+def prediction_by_randomfgridsearch(model, x_test):
+    """
+        Makes predictions given a model trained with a random forest algorithm
+        :param model: the pre-trained model
+        :param x_test: the measurements of the test portion of the dataset
+        :return: the predictions
+        """
+    # Make predictions
+    print("\n## INFO: starting predictions")
+    predictions = model.predict(x_test)
+    predictions = np.round(predictions)
+    return predictions
+
+
+
 def classification_by_xgboost(x_train, y_train):
     """
-    Trains a model using the xgboost algorithm for better performances
-    :param x_train: the measurements for the training
-    :param y_train: the labels for the training
-    :return: the trained model
+
+    :param x_train:
+    :param y_train:
+    :return:
     """
-    # Compute hyperparameters
-    params = {"objective": "reg:squarederror", "tree_method": "gpu_hist"}
+    # Hyperparameters
+    params = {"objective": "reg:squarederror", "tree_method": "hist"}
+
 
     # Create a DMatrix based on x_train
     d_train = xgb.DMatrix(x_train, y_train, enable_categorical=True)
@@ -240,10 +257,10 @@ def classification_by_xgboost(x_train, y_train):
 
 def prediction_by_xgboost(model, x_test, y_test):
     """
-    Makes predictions given a model trained with a random forest algorithm
-    :param model: the pre-trained model
-    :param x_test: the measurements of the test portion of the dataset
-    :return: the predictions
+
+    :param model:
+    :param x_test:
+    :return:
     """
     # Create a DMatrix based on x_train
     d_test = xgb.DMatrix(x_test, y_test, enable_categorical=True)
@@ -292,22 +309,19 @@ def classification_by_lstm(x_train, y_train):
 
     # Convert numpy arrays to tensors
     print("\n## INFO: converting to tensors")
-
-    # Create a new list of tensors
     new_x = []
-
-    # For each value in x_train, convert it into a Tensor for the model
     for i in tqdm(range(len(x_train))):
         tensor = tf.convert_to_tensor(x_train[i])
         new_x.append(tensor)
 
     x_train = new_x
 
-    # Stack values for fulfilling LSTM standard input
+    # Fitting tensorflow model
+    print("## INFO: fitting model...")
+
     x_train = tf.stack(x_train)
     y_train = tf.stack(y_train)
 
-    # Fit LSTM
     model.fit(x_train, y_train, epochs=30, batch_size=150, verbose=1)
 
     return model
@@ -322,11 +336,7 @@ def prediction_by_lstm(model, x_test):
     """
     # Convert numpy arrays to tensors
     print("\n## INFO: converting to tensors")
-
-    # Create a new list of tensors
     new_x = []
-
-    # For each value in x_test, convert it into a Tensor for the model
     for i in tqdm(range(len(x_test))):
         tensor = tf.convert_to_tensor(x_test[i])
         new_x.append(tensor)
@@ -334,6 +344,7 @@ def prediction_by_lstm(model, x_test):
     x_test = new_x
 
     # Check predicted values
+    print("## INFO: Starting prediction...")
     predictions = model.predict(x_test)
 
     # Undo scaling
@@ -350,62 +361,46 @@ def compute_confusion_matrix(predictions, correct_labels):
     :return: a dictionary containing keys as couples (p, c) i.e. predicted class and correct class, and values as the counters of each instance of (p, c)
     """
 
-    # Create a dictionary where:
-    # the key is a tuple (p, c) where p is the predicted value and c is the correct label;
-    # the value is the number of times the model predicted p and the expected prediction was c.
+    # Create a dictionary for which the key is the tuple (p, c) where p is the predicted value and c is the correct label and the value
+    # is the number of times the model predicted p and the expected prediction was c.
     # If p == c then the model predicted the correct value.
     confusion_matrix = dict()
 
-    # For each prediction
     for i in range(len(predictions)):
-        # p is the predicted value
         p = predictions[i].item()
-
-        # c is the expected value
         c = correct_labels[i].item()
 
-        # (p, c) will be the key of the dictionary
         key = (p, c)
 
         if not key in confusion_matrix.keys():
-            # If (p, c) still doesn't exist in the dictionary create a new entry
-            confusion_matrix[key] = 1
+            confusion_matrix[key] = 0
         else:
-            # Otherwise increase the number of times the model predicted p and the correct label was c
             confusion_matrix[key] += 1
 
     return confusion_matrix
-
 
 def compute_metrics(confusion_matrix):
     """
     Calculates accuracy, precision, recall and fscore for each class of the confusion matrix.
     :param confusion_matrix: the confusion matrix previously computed
-    :return: a dictionary of key, values where keys are the classes and values are tuples like (accuracy, precision, recall, fscore)
+    :return: a dictionary of key, values where keys are the classes and values are tuples like (accuracy, precision, recall, f-score)
     """
 
-    # Identify all the classes that should've been predicted
+    # Identify all the classes
     classes = set()
-
-    # For each entry of the confusion matrix find the classes of predictions
     for k in confusion_matrix.keys():
-        # The second element of the key is the expected class
         _, c = k
         classes.add(c)
 
     # Create a dictionary in which keys are the classes and values are (Accuracy, Precision, Recall, F-Score)
     metrics = dict()
 
-    # For each class compute the 4 standard metrics
     for cl in classes:
-
-        # Initialize the number of true positives, true negatives, false positives and false negatives
+        # For each class we are computing TP, TN, FP, FN
         tp = tn = fp = fn = 0
-
-        # For each prediction, compare the predicted class and the expected class and increase the relative counter
         for k, v in confusion_matrix.items():
             p, c = k
-            if p == c and p == cl:
+            if p == c:
                 tp += v
             elif p == cl and c != cl:
                 fp += v
@@ -414,7 +409,6 @@ def compute_metrics(confusion_matrix):
             elif p != cl and c != cl:
                 tn += v
 
-        # Avoid division by zero
         if tp == tp == fp == fn == 0:
             acc = pr = recall = fscore = 0
         else:
@@ -423,7 +417,6 @@ def compute_metrics(confusion_matrix):
             recall = (tp / (tp + fn)) * 100
             fscore = (2 * recall * pr) / (recall + pr) / 100
 
-        # Create an entry in the dictionary for the current class
         metrics[cl] = (acc, pr, recall, fscore)
 
     return metrics
